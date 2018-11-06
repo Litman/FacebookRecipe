d2fbfe28e25e8782d80a958eb6df593595200055
L0002623\vector <layala@vectoritcgroup.com>
2018-11-06 09:20:06 -0500

    build recipe list activity

--- app/src/main/java/ghostl/com/facebookrecipesexample/RecipeListActivity.java
+++ app/src/main/java/ghostl/com/facebookrecipesexample/RecipeListActivity.java
@@ -1,13 +1,129 @@
 package ghostl.com.facebookrecipesexample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class RecipeListActivity extends AppCompatActivity {
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ghostl.com.facebookrecipesexample.entities.Recipe;
+import ghostl.com.facebookrecipesexample.recipelist.RecipeListPresenter;
+import ghostl.com.facebookrecipesexample.recipelist.ui.RecipeListView;
+import ghostl.com.facebookrecipesexample.recipelist.ui.adapters.OnItemClickListener;
+import ghostl.com.facebookrecipesexample.recipelist.ui.adapters.RecipesAdapter;
+import ghostl.com.facebookrecipesexample.recipemain.ui.RecipeMainActivity;
+
+public class RecipeListActivity extends AppCompatActivity implements RecipeListView, OnItemClickListener{
+
+    @Bind(R.id.tbListRecipe)
+    Toolbar tbListRecipe;
+    @Bind(R.id.rvListRecipe)
+    RecyclerView rvListRecipe;
+
+    RecipesAdapter adapter;
+    RecipeListPresenter listPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+        ButterKnife.bind(this);
+
+        setupToolbar();
+        setupRecyclerView();
+        setupInjection();
+        listPresenter.onCreate();
+        listPresenter.getRecipes();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        listPresenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recipes_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_main){
+            navigateToMainScreen();
+        }else if(id == R.id.action_logout){
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FacebookRecipesApp app = (FacebookRecipesApp) getApplication();
+        app.logout();
+    }
+
+    private void navigateToMainScreen() {
+        startActivity(new Intent(this, RecipeMainActivity.class));
+    }
+
+    private void setupInjection() {
+    }
+
+    private void setupRecyclerView() {
+        rvListRecipe.setLayoutManager(new GridLayoutManager(this, 2));
+        rvListRecipe.setAdapter(adapter);
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(tbListRecipe);
+    }
+
+    @OnClick(R.id.tbListRecipe)
+    public void onToolbarClick(){
+        rvListRecipe.smoothScrollToPosition(0);
+    }
+
+
+    @Override
+    public void setRecipes(List<Recipe> data) {
+        adapter.setRecipes(data);
+    }
+
+    @Override
+    public void recipeUpdated() {
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void recipeDeleted(Recipe recipe) {
+        adapter.removeRecipe(recipe);
+    }
+
+    @Override
+    public void onFavClick(Recipe recipe) {
+        listPresenter.toggleFavorite(recipe);
+    }
+
+    @Override
+    public void onItemClick(Recipe recipe) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(recipe.getSourceURL()));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDeleteClick(Recipe recipe) {
+        listPresenter.removeRecipe(recipe);
     }
 }
--- app/src/main/java/ghostl/com/facebookrecipesexample/recipelist/ui/adapters/RecipesAdapter.java
+++ app/src/main/java/ghostl/com/facebookrecipesexample/recipelist/ui/adapters/RecipesAdapter.java
@@ -1,14 +1,21 @@
 package ghostl.com.facebookrecipesexample.recipelist.ui.adapters;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareButton;
 
 import java.util.List;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import ghostl.com.facebookrecipesexample.R;
 import ghostl.com.facebookrecipesexample.entities.Recipe;
 import ghostl.com.facebookrecipesexample.libs.base.ImageLoader;
@@ -35,28 +42,85 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.ViewHold
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Recipe currenteRecipe = recipeList.get(position);
-        //imageLoader.load(holder.);
+        imageLoader.load(holder.ivRecipe, currenteRecipe.getImageURL());
+        holder.tvRecipeName.setText(currenteRecipe.getTitle());
+        holder.ibFavorite.setTag(currenteRecipe.getFavorite());
+        if(currenteRecipe.getFavorite()){
+            holder.ibFavorite.setImageResource(android.R.drawable.btn_star_big_on);
+
+        }else{
+            holder.ibFavorite.setImageResource(android.R.drawable.btn_star_big_off);
+        }
+
+        holder.setOnItemClickListener(currenteRecipe, onItemClickListener);
     }
 
     @Override
     public int getItemCount() {
         return recipeList.size();
     }
-    private void setRecipes(List<Recipe> recipes){
+    public void setRecipes(List<Recipe> recipes){
         this.recipeList = recipes;
         notifyDataSetChanged();
     }
 
-    private void removeRecipe(Recipe recipe){
+    public void removeRecipe(Recipe recipe){
         recipeList.remove(recipe);
         notifyDataSetChanged();
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder
     {
+        @Bind(R.id.ivRecipe)
+        ImageView ivRecipe;
+        @Bind(R.id.tvRecipeName)
+        TextView tvRecipeName;
+        @Bind(R.id.ibFavorite)
+        ImageButton ibFavorite;
+        @Bind(R.id.ibDelete)
+        ImageButton ibDelete;
+        @Bind(R.id.bfShare)
+        ShareButton bfShare;
+        @Bind(R.id.bfSend)
+        ShareButton bfSend;
+
+        private View view;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            this.view = itemView;
+            ButterKnife.bind(this, view);
+        }
+
+        public void setOnItemClickListener(final Recipe currenteRecipe, final OnItemClickListener onItemClickListener) {
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onItemClick(currenteRecipe);
+                }
+            });
+
+            ibFavorite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onDeleteClick(currenteRecipe);
+                }
+            });
+
+            ibDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onDeleteClick(currenteRecipe);
+                }
+            });
+
+            ShareLinkContent content = new ShareLinkContent.Builder()
+                    .setContentUrl(Uri.parse(currenteRecipe.getSourceURL()))
+                    .build();
+
+            bfShare.setShareContent(content);
+            bfSend.setShareContent(content);
         }
     }
 }
